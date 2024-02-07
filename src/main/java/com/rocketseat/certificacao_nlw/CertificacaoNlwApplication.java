0c5a20f6197fd12e7108ba9a7052dfc0cc768c65
Viviane Mezzomo <viviane.mezzomo@codex.com.br>
2024-02-06 22:25:31 -0300

    Commit inicial

---
+++ src/main/java/com/rocketseat/certificacao_nlw/CertificacaoNlwApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.certificacao_nlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificacaoNlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificacaoNlwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketseat/certificacao_nlw/CertificacaoNlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.certificacao_nlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificacaoNlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
